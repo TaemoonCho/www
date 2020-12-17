37a01ad65a50f4b3d60629d032e6a47585f32462
Jake Taemoon Jo <whxoans@gmail.com>
2020-12-17 18:48:49 +0700

    init commit

---
+++ replaceWordsForSwagger.js
@@ -0,0 +1,23 @@
+var elementToObserve = document.getElementsByClassName("model-box");
+var observer = new MutationObserver(function (mutationsList, observer) {
+    var properties = document.getElementsByClassName("property");
+    var arr = [].slice.call(properties);
+    properties = arr.filter((property) => property.className == "property");
+    properties.forEach((property) => {
+        property.innerText = property.innerText
+            .replace("OrderedMap ", "")
+            .replace(
+                /, "\$\$ref"\: "#\/components\/examples\/([A-Za-z].*)\"/g,
+                "",
+            );
+    });
+});
+for (let i = 0; i < elementToObserve.length; i++) {
+    if (elementToObserve[i].tagName == "SPAN") {
+        observer.observe(elementToObserve[i], {
+            characterData: true,
+            childList: true,
+            attributes: true,
+        });
+    }
+}
