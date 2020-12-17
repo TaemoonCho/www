3c0b5e7241a952daa035df56e8d2765132bd3894
Jake Taemoon Jo <whxoans@gmail.com>
2020-12-17 19:20:06 +0700

    Use mutation in load

--- replaceWordsForSwagger.js
+++ replaceWordsForSwagger.js
@@ -1,32 +1,37 @@
-console.log("Jake Taemoon Jo");
-var rootSwaggerDiv = document.getElementById("swagger-ui");
-var rootObserver = new MutationObserver(function (mutationsList, observer) {
-    var elementToObserve = document.getElementsByClassName("model-box");
-    var observer = new MutationObserver(function (mutationsList, observer) {
-        var properties = document.getElementsByClassName("property");
-        var arr = [].slice.call(properties);
-        properties = arr.filter((property) => property.className == "property");
-        properties.forEach((property) => {
-            property.innerText = property.innerText
-                .replace("OrderedMap ", "")
-                .replace(
-                    /, "\$\$ref"\: "#\/components\/examples\/([A-Za-z].*)\"/g,
-                    "",
-                );
-        });
-    });
-    for (let i = 0; i < elementToObserve.length; i++) {
-        if (elementToObserve[i].tagName == "SPAN") {
-            observer.observe(elementToObserve[i], {
-                characterData: true,
-                childList: true,
-                attributes: true,
+document.addEventListener("load", function () {
+    var rootSwaggerDiv = document.getElementById("swagger-ui");
+    var rootObserver = new MutationObserver(function (mutationsList, observer) {
+        console.log("Jake Taemoon Jo");
+        var elementToObserve = document.getElementsByClassName("model-box");
+        var observer = new MutationObserver(function (mutationsList, observer) {
+            var properties = document.getElementsByClassName("property");
+            var arr = [].slice.call(properties);
+            properties = arr.filter(
+                (property) => property.className == "property",
+            );
+            properties.forEach((property) => {
+                property.innerText = property.innerText
+                    .replace("OrderedMap ", "")
+                    .replace(
+                        /, "\$\$ref"\: "#\/components\/examples\/([A-Za-z].*)\"/g,
+                        "",
+                    );
             });
+        });
+        for (let i = 0; i < elementToObserve.length; i++) {
+            if (elementToObserve[i].tagName == "SPAN") {
+                observer.observe(elementToObserve[i], {
+                    characterData: true,
+                    childList: true,
+                    attributes: true,
+                });
+            }
         }
-    }
+    });
+    rootObserver.observe(rootSwaggerDiv, {
+        characterData: true,
+        childList: true,
+        attributes: true,
+    });
 });
-rootObserver.observe(rootSwaggerDiv, {
-    characterData: true,
-    childList: true,
-    attributes: true,
-});
+
